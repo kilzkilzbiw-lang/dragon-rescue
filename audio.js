10f9b9459c044e607a7dbfd45cb8c440bed82c71
kilzkilzbiw-lang <kilz.kilzbiw@gmail.com>
2026-03-10 01:30:36 +0800

    Add background music and sound effects initialization

---
+++ audio.js
@@ -0,0 +1,7 @@
+let bgm = new Audio("audio/bgm.mp3")
+bgm.loop = true
+bgm.volume = 0.4
+
+let fireSound = new Audio("audio/fire.wav")
+let hitSound = new Audio("audio/hit.wav")
+let winSound = new Audio("audio/win.wav")
